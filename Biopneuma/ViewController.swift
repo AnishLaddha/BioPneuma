6f80ae719a56ed0ee4927f9005ea5872a889d84b
Anish Laddha <anishladdha03@gmail.com>
2020-03-29 14:52:41 -0700

    first commit

---
+++ Biopneuma/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Biopneuma
+//
+//  Created by Ishan Jain on 3/27/20.
+//  Copyright © 2020 Ishan Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 10
+    }
+
+
+}
